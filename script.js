72239480ffeb578312ff0bd166d4654fd5709875
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-11-19 16:46:20 +0100

    Creazione prima funzione
    - Ho creato l'array vuoto  
    - Ho creato la funzione generateRandomBombs
    - Questa genera 16 numeri all'interno di un min e un max e li pusha nell'array

--- script.js
+++ script.js
@@ -10,3 +10,22 @@
 // 1 Creo un array di bombe vuoto
 // 2 Creo una funzione che genera 16 numeri casuali e li pusha nell'array
 // 3 Creo una funzione che vada a verificare se il numero inserito dall'utente è già presente nell'array
+// ATTENZIONE: PER VELOCIZZARMI CON LA CREAZIONE DELL'ESERCIZIO, SOLO MOMENTANEAMENTE USO NUMERI PIU' PICCOLI
+
+// Mi sa che devo togliere il var così l'array diventa universale e viene riconosciuto anche dalle funzioni se richiamato (così posso pusharci richiamandolo)
+var arrayBombs = [];
+generateRandomBombs(1,12)
+console.log(arrayBombs);
+
+// FUNZIONI
+function generateRandomBombs(min, max) {
+  // La bomba è una variabile locale poichè dev'essere riconosciuta solo dall'array
+  // Devo usare un while così genero numeri finchè l'array non arriva a lunghezza 16
+  // La sua variabile inizializzata è arrayBombs
+  do {
+    var bomb = Math.ceil(Math.random()*max);
+    arrayBombs.push(bomb);
+  } while (arrayBombs.length < 16);
+}
+
+function checkForBomb(number, array) {}
