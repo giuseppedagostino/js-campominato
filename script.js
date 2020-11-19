0c2db8d08d147eaf36534b26056a80008e903a59
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-11-19 16:09:04 +0100

    Definizione step iniziali

--- script.js
+++ script.js
@@ -6,3 +6,7 @@
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+// 1 Creo un array di bombe vuoto
+// 2 Creo una funzione che genera 16 numeri casuali e li pusha nell'array
+// 3 Creo una funzione che vada a verificare se il numero inserito dall'utente è già presente nell'array
